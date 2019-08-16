012f0ec7a53fcd11c963d917cd90e9627ea8a32e
Muhammad Ubaid Raza <mubaidr@gmail.com>
2019-08-16 22:47:29 +0500

    add build

---
+++ precache-manifest.0b21198369efb57cf05d8e7e1c784a46.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c532d6c6069cf3aff919",
+    "url": "css/app.css"
+  },
+  {
+    "revision": "c532d6c6069cf3aff919",
+    "url": "js/app.js"
+  },
+  {
+    "revision": "cf4f9363fcc57ef30a7b",
+    "url": "js/chunk-vendors.js"
+  },
+  {
+    "revision": "3b80a38ca50ed1ca698a",
+    "url": "css/examples.css"
+  },
+  {
+    "revision": "3b80a38ca50ed1ca698a",
+    "url": "js/examples.js"
+  },
+  {
+    "revision": "14a51a0f5a6de79a750c",
+    "url": "js/getting-started.js"
+  },
+  {
+    "revision": "191a9744bd8a1bd91caa",
+    "url": "js/tutorial.js"
+  },
+  {
+    "revision": "2773d2d1923fe87a0d353116ddeb682a",
+    "url": "img/logo.svg"
+  },
+  {
+    "revision": "1ae6f801cfb252ae67b538fde2098c49",
+    "url": "index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
