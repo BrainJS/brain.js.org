9559d20c342c475d14e5b0b735d2dd961d766724
Muhammad Ubaid Raza <mubaidr@gmail.com>
2019-10-07 21:58:28 +0500

    fix footer link: https://github.com/BrainJS/brain.js.org/issues/6

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,9 @@
         The source code is licensed
         <a href="//opensource.org/licenses/mit-license.php">MIT</a>. <br />
         This page is open source. Noticed a typo? Or something unclear?
-        <a href="//github.com/BrainJS/brain.js">Improve this page on GitHub</a>
+        <a href="//github.com/BrainJS/brain.js.org"
+          >Improve this page on GitHub</a
+        >
       </p>
     </div>
   </footer>
